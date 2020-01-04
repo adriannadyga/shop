ae0025d6f7a4f4c09cc615203bc9f98ab8d288da
Adrianna Dyga <adrianna.dyga@gmail.com>
2020-01-04 13:17:42 +0100

    Add init files

---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+import 'animate.css/animate.min.css';
+
+const Root = () => (
+    <App />
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
